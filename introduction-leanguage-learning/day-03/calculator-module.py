893fe14fa16bf9a2f604702cb8c5c2a5de1ad539
Esteban Casallas <ecasallas07@gmail.com>
2024-02-28 23:25:34 -0500

    code of string

---
+++ introduction-leanguage-learning/day-03/calculator-module.py
@@ -0,0 +1,5 @@
+# calculator of module 
+mod = int(input("Enter your number for module: "))
+num =  int(input("Enter your number: "))
+
+print(num % mod)
---
+++ introduction-leanguage-learning/day-04/strings.py
@@ -0,0 +1,43 @@
+# single line coment
+sentence = "I hope you are enjoying 30 days of python challenge"
+print(sentence)
+
+phrase = "Hello, World!"
+print(phrase)
+
+# Multiline string
+
+text = """
+    I am learning englhis every day
+"""
+print(text)
+
+
+#### Unpacking characters 
+
+language = 'Python'
+a,b,c,d,e,f = language # unpacking sequence characters into variables
+print(a) # P
+print(b) # y
+print(c) # t 
+print(d) # h
+print(e) # o
+print(f) # n
+
+
+# comment lines
+"""
+    Comments for word
+"""
+
+# accesing a characters
+
+language = "Python"
+print(language[0])
+last_index = len(language) -1 
+
+# Challengue capitalize 
+# Convert first lette to Capital letter
+challengue = "when are you"
+print(challengue.capitalize())  # When are you
+
